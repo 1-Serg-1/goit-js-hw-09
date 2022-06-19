8b4d836669e34ccb7faced0c4940e748e2d3b573
1-Serg-1 <1.serhiiboiko.1@gmail.com>
2022-06-19 23:30:41 +0300

    Task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
 
+refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]')
+}
+
+refs.btnStart.addEventListener('click', onStart);
+refs.btnStop.addEventListener('click', onStop);
+
+let intervalId;
+
+function onStart() {
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+
+  intervalId = setInterval(changeColorBody, 1000);
+}
+
+function onStop() {
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+  clearInterval(intervalId);
+}
+
+function changeColorBody() {
+  const currentColor = getRandomHexColor();
+  refs.btnStart.parentElement.style.background = `${currentColor}`;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
